d7689b8b78d9a00c42f9abe57e19961ce434534c
HubertSia <hubemail2021@gmail.com>
2024-09-07 15:19:47 -0400

    Added the ''drawing'' file module

---
+++ topics/instructions/drawing/js/script.js
@@ -0,0 +1,77 @@
+/**
+ * Function calls
+ * Hubert Sia
+ * 
+ * Taught by Pippin Barr
+ * 
+ * 
+ *  * The Blank Page
+ * Pippin Barr
+ * 
+ * An exploration of the existential angst of a novelist confronting
+ * the blank page while under huge pressure from their publisher. The
+ * program is non-interactive intentionally in order to simulate the
+ * writer's inability to get started on the project.
+ * 
+ * Uses:
+ * p5.js
+ * https://p5js.org/
+ * 
+ * 
+ * 
+ */
+
+"use strict";
+
+
+    // Once at the beginning of the program
+function setup() {
+    
+
+
+/**
+ * Creates a 640x480 canvas for our masterpiece
+ */
+
+    // Creates a canvas (width, height)
+    // Create the canvas at a standard resolution
+    createCanvas(640, 480);
+
+ //=================================================   
+
+    // Base canvas for the smile
+    // createCanvas(100, 100);
+
+}
+
+
+
+    // Every frame of the program
+function draw() {
+    
+
+/**
+ * Draws a blank piece of paper on a pink background
+ */
+
+
+    // Fills the background of the canvas with color (R,G,B)
+    // Pink background
+    background(255, 100, 100);
+
+    // Generates a rectangle(x, y, witdth, height )
+    // The blank piece of paper
+    rect(200, 80, 240, 320);
+
+//======================================================
+
+   /** 
+    //the eyes
+    point(20,20);
+    point(70,20);
+    
+    //the mouth
+    line(20, 60, 70, 60);*/
+
+
+}
