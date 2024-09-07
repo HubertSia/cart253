06cc78fa478f391b4d4ec25d85af87ee4a02f8a1
HubertSia <hubemail2021@gmail.com>
2024-09-07 15:02:44 -0400

    Completed: Function calls modules aka simulation of an writer with writer's block

--- topics/instructions/function-calls/js/script.js
+++ topics/instructions/function-calls/js/script.js
@@ -1,24 +1,45 @@
 /**
- * Title of Project
- * Author Name
+ * Function calls
+ * Hubert Sia
  * 
- * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
- * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
+ * Taught by Pippin Barr
+ * 
+ * 
+ * 
  */
 
 "use strict";
 
-/**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
-*/
+
+    // Once at the beginning of the program
 function setup() {
+    
+    //Creates a canvas (width, height)
+    createCanvas(640, 480);
+
+    //Base canvas for the smile
+    //createCanvas(100, 100);
 
 }
 
 
-/**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
-*/
+
+    // Every frame of the program
 function draw() {
+    
+    // Fills the background of the canvas with color (R,G,B)
+    background(255, 100, 100);
+
+    // Generates a rectangle(x, y, witdth, height )
+    rect(200, 80, 240, 320);
+
+   /** 
+    //the eyes
+    point(20,20);
+    point(70,20);
+    
+    //the mouth
+    line(20, 60, 70, 60);*/
+
 
 }
