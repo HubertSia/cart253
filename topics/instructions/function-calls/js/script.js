155df15b292b06abaabe13cefc3b2490cf785c47
HubertSia <hubemail2021@gmail.com>
2024-09-07 15:17:34 -0400

    Completed: Commenting Modules

--- topics/instructions/function-calls/js/script.js
+++ topics/instructions/function-calls/js/script.js
@@ -5,6 +5,19 @@
  * Taught by Pippin Barr
  * 
  * 
+ *  * The Blank Page
+ * Pippin Barr
+ * 
+ * An exploration of the existential angst of a novelist confronting
+ * the blank page while under huge pressure from their publisher. The
+ * program is non-interactive intentionally in order to simulate the
+ * writer's inability to get started on the project.
+ * 
+ * Uses:
+ * p5.js
+ * https://p5js.org/
+ * 
+ * 
  * 
  */
 
@@ -14,11 +27,20 @@
     // Once at the beginning of the program
 function setup() {
     
-    //Creates a canvas (width, height)
+
+
+/**
+ * Creates a 640x480 canvas for our masterpiece
+ */
+
+    // Creates a canvas (width, height)
+    // Create the canvas at a standard resolution
     createCanvas(640, 480);
 
-    //Base canvas for the smile
-    //createCanvas(100, 100);
+ //=================================================   
+
+    // Base canvas for the smile
+    // createCanvas(100, 100);
 
 }
 
@@ -27,11 +49,21 @@ function setup() {
     // Every frame of the program
 function draw() {
     
+
+/**
+ * Draws a blank piece of paper on a pink background
+ */
+
+
     // Fills the background of the canvas with color (R,G,B)
+    // Pink background
     background(255, 100, 100);
 
     // Generates a rectangle(x, y, witdth, height )
+    // The blank piece of paper
     rect(200, 80, 240, 320);
+
+//======================================================
 
    /** 
     //the eyes
