e6f46d4d65c7f33288310713e086cacad7aa7e0c
HubertSia <hubemail2021@gmail.com>
2024-09-07 20:04:52 -0400

    Completed: Debugging module (took me five minutes)

--- topics/instructions/debugging-instructions/js/script.js
+++ topics/instructions/debugging-instructions/js/script.js
@@ -13,7 +13,7 @@
  * Creates the canvas
 */
 function setup() {
-    createCanva(500, 500);
+    createCanvas(500, 500);
 }
 
 
@@ -22,16 +22,20 @@ function setup() {
 */
 function draw() {
     // Pink background
-    background(255, 150);
+    background(255, 150, 170);
 
     // Draw the bug
     drawBug();
 }
 
-function drawInsect() {
+/**
+ * Draws the bug!
+ */
+function drawBug() {
     drawBody();
     drawAntennae();
     drawEye();
+    drawLegs();
 }
 
 /**
@@ -61,22 +65,23 @@ function drawLegs() {
 /**
  * Draws the antennae of the bug
  */
-function drawAntennae({
+function drawAntennae(){
     push();
-strok(0);
-strokeWeight(5);
-line(250, 250, 200, 50);
-line(250, 250, 300, 50);
-pop();
+    stroke(0);
+    strokeWeight(5);
+    line(250, 250, 200, 50);
+    line(250, 250, 300, 50);
+    pop();
 }
 
 /**
  * Draws the eyes of the bug
  */
-function drawEyes() {
+function drawEye() {
     push();
     fill(255, 0, 0);
     noStroke();
     ellipse(200, 150, 15);
     ellipse(300, 150, 15);
     pop();
+}
