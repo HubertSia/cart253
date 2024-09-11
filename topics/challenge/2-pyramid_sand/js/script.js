2afd361e9d9b96d43052a5dab5424e625d1e1750
HubertSia <hubemail2021@gmail.com>
2024-09-10 22:12:00 -0400

    name change

--- topics/challenge/2-pyramid_sand/js/script.js
+++ topics/challenge/2-pyramid_sand/js/script.js
@@ -15,6 +15,8 @@ function setup() {
 function draw() {
         background(99, 140, 250);
         sand();
+        pyramid();
+        pyramid2();
         
 }
 
@@ -22,8 +24,25 @@ function sand(){
 
     //push();
     ellipse(0, 600, 725, 590);
-    ellipse(590, 750, 870, 590);
+    ellipse(590, 750, 970, 690);
     noStroke();
-    fill(255, 255, 0)
+    fill(255, 210, 0); 
     //pop();
+}
+
+
+
+function pyramid(){
+
+    //push();
+    triangle(185, 485, 390, 530, 325, 287);
+    fill(128, 128, 128);
+    noStroke();
+    //pop();
+}
+
+function pyramid2(){
+    triangle(390, 530, 485, 460, 325, 287);
+    fill(255, 255, 0);    
+    noStroke();
 }
