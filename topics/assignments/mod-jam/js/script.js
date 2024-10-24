73bebcd1ea18f023f81adf1f98c03127bba35af1
HubertSia <hubemail2021@gmail.com>
2024-10-24 11:58:40 -0700

    revamp frog and fly names to moshi and applewings respectivally

--- topics/assignments/mod-jam/js/script.js
+++ topics/assignments/mod-jam/js/script.js
@@ -1,54 +1,20 @@
 /**
  * Moshi vs Browser
- * Pippin Barr
+ * Hubert Sia
  * 
- * # Planning
+ * Moshi th 
+ * 
+ * Instructions:
+ * Moshi (bootleg Yoshi) eating apple-wings
+ * Not letting Browser (bootleg Bowser) grapping 10 apple-wings
 
-## Starting point
-
-The initial idea:
-
-> Moshi (bootleg Yoshi) eating apple-wings
-> Not letting Browser (bootleg Bowser) grapping 10 apple-wings
-
-## Experience design
-
-The experience:
-
-> The user plays as Moshi the friendly dinosaur. Their goal is to consume 10 apple-wings that will move on screen
-> by shooting the tongue with a voice mic. They have to avoid 10 misses or else Browser will win the game
-
-## Breaking it down
-
-Basic things to do:
-
-- Draw the Moshi (Bascically draw the head of bootleg Yoshi)
-- Draw Moshi's tongue
-- Move the Moshi with the mouse movement (X axis)
-- Move the apple-wings, it will spawn on both edges of the canvas (moving in a random Y Axis, and flying on a linear direction)
-- Figure out if the tongue hits the fly?
-- Adding a score points of 10
-
-Questions:
-
-- What does the Moshi look like?
-  - He is composes of a lot of circles and oval ellipse
-- How does the user control the Moshi?
-  - User controls the dinosaur with the mouse position, just to the left and right
-  - User launches the tongue with a mic sounds. They need to shout to the mic in order to launch the TONGUE
-- How does the apple-wings move?
-  - The apple-wings starts on the left at a random y position, and moves to the right in a line
-  - It will also starts on the right at a random y position, and moves to the left in a line
-- What does the tongue look like?
-  - A red line coming out from Moshi
-- What happens if the user doesn't consume the apple-wings?
-  - If the apple-wings from the left/right goes off the right/left side, it just resets to a new random y on the left/right.
-  - If the players contiues to miss 10 times. It will be a game over, showing Browser with a mencacing look
-- What does it all look like on the screen? Layout?
-  - At the start, they will be a menu screen, giving the intro information on how it works
-  - In the game, Moshi at the bottom, fly moving across, tongue shooting out of frog
-  - Moshi must consume as much apple-wings as possible before Browser reaches to 10
-  - If the players miss the apple-wings 10 times. It will be consider a game over
+ * Idea:
+ * With the Frogfrogfrog project as our base template. It's going to be revamp game with a bootleg characters of Yoshi and Bowser
+ * A title with instructions, and ending screen.\
+ *A mic control system wich allows the players to shout to activate the tongue. (Suggested to use the map function)
+ *The sound plays when ate the flying apples
+ *Apple wings spawns at a random positions (from left to right or right to left)
+ *Make sure the players grab as much the apple-wings as possible. If players miss 10 apples, Browser wins a it is consider a game over.
  * 
  * 
  * Made with p5
@@ -58,7 +24,7 @@ Questions:
 "use strict";
 
 // Our frog
-const frog = {
+const moshi = {
     // The frog's body has a position and size
     body: {
         x: 320,
@@ -76,9 +42,17 @@ const frog = {
     }
 };
 
-// Our fly
+const browser = {
+    // The frog's body has a position and size
+    body: {
+        x: 320,
+        y: 520,
+        size: 150
+    }
+}
+// Our applewing
 // Has a position, size, and speed of horizontal movement
-const fly = {
+const applewing = {
     x: 0,
     y: 200, // Will be random
     size: 10,
@@ -101,7 +75,7 @@ function setup() {
     createCanvas(640, 480);
 
     // Give the fly its first random position
-    resetFly();
+    resetAppleFly();
 }
 
 function draw() {
@@ -109,9 +83,9 @@ function draw() {
         title();
     }else if( state === "game" ){
         game();
-    }else if (state === "game-over"){
-        
-    }
+    }else if( state === "gameover" ){
+    gameover();
+}
 }
 
 
@@ -134,13 +108,13 @@ function title(){
 function game(){
     
     background("#87ceeb");
-    moveFly();    
+    moveAppleWing();    
     moveTongue();
-    checkTongueFlyOverlap();
-    drawFly();
+    checkTongueAppleWingOverlap();
+    drawAppleWing();
     drawScore();    
-    moveFrog();
-    drawFrog();
+    moveMoshi();
+    drawMoshi();
 
 
 
@@ -151,23 +125,23 @@ function game(){
  * Moves the fly according to its speed
  * Resets the fly if it gets all the way to the right
  */
-function moveFly() {
+function moveAppleWing() {
     // Move the fly
-    fly.x += fly.speed;
+    applewing.x += applewing.speed;
     // Handle the fly going off the canvas
-    if (fly.x > width) {
-        resetFly();
+    if (applewing.x > width) {
+        resetAppleFly();
     }
 }
 
 /**
  * Draws the fly as a black circle
  */
-function drawFly() {
+function drawAppleWing() {
     push();
     noStroke();
     fill("#000000");
-    ellipse(fly.x, fly.y, fly.size);
+    ellipse(applewing.x, applewing.y, applewing.size);
     pop();
 }
 
@@ -177,16 +151,16 @@ function drawFly() {
 /**
  * Resets the fly to the left with a random y
  */
-function resetFly() {
-    fly.x = 0;
-    fly.y = random(0, 300);
+function resetAppleFly() {
+    applewing.x = 0;
+    applewing.y = random(0, 300);
 }
 
 /**
  * Moves the frog to the mouse position on x
  */
-function moveFrog() {
-    frog.body.x = mouseX;
+function moveMoshi() {
+    moshi.body.x = mouseX;
 }
 
 /**
@@ -194,25 +168,25 @@ function moveFrog() {
  */
 function moveTongue() {
     // Tongue matches the frog's x
-    frog.tongue.x = frog.body.x;
+    moshi.tongue.x = moshi.body.x;
     // If the tongue is idle, it doesn't do anything
-    if (frog.tongue.state === "idle") {
+    if (moshi.tongue.state === "idle") {
         // Do nothing
     }
     // If the tongue is outbound, it moves up
-    else if (frog.tongue.state === "outbound") {
-        frog.tongue.y += -frog.tongue.speed;
+    else if (moshi.tongue.state === "outbound") {
+        moshi.tongue.y += -moshi.tongue.speed;
         // The tongue bounces back if it hits the top
-        if (frog.tongue.y <= 0) {
-            frog.tongue.state = "inbound";
+        if (moshi.tongue.y <= 0) {
+            moshi.tongue.state = "inbound";
         }
     }
     // If the tongue is inbound, it moves down
-    else if (frog.tongue.state === "inbound") {
-        frog.tongue.y += frog.tongue.speed;
+    else if (moshi.tongue.state === "inbound") {
+        moshi.tongue.y += moshi.tongue.speed;
         // The tongue stops if it hits the bottom
-        if (frog.tongue.y >= height) {
-            frog.tongue.state = "idle";
+        if (moshi.tongue.y >= height) {
+            moshi.tongue.state = "idle";
         }
     }
 }
@@ -220,37 +194,37 @@ function moveTongue() {
 /**
  * Displays the tongue (tip and line connection) and the frog (body)
  */
-function drawFrog() {
+function drawMoshi() {
     // Draw the tongue tip
     push();
     fill("#ff0000");
     noStroke();
-    ellipse(frog.tongue.x, frog.tongue.y, frog.tongue.size);
+    ellipse(moshi.tongue.x, moshi.tongue.y, moshi.tongue.size);
     pop();
 
     // Draw the rest of the tongue
     push();
     stroke("#ff0000");
-    strokeWeight(frog.tongue.size);
-    line(frog.tongue.x, frog.tongue.y, frog.body.x, frog.body.y);
+    strokeWeight(moshi.tongue.size);
+    line(moshi.tongue.x, moshi.tongue.y, moshi.body.x, moshi.body.y);
     pop();
 
     // Draw the frog's body
     push();
     fill("#00ff00");
     noStroke();
-    ellipse(frog.body.x, frog.body.y, frog.body.size);
+    ellipse(moshi.body.x, moshi.body.y, moshi.body.size);
     pop();
 }
 
 /**
  * Handles the tongue overlapping the fly
  */
-function checkTongueFlyOverlap() {
+function checkTongueAppleWingOverlap() {
     // Get distance from tongue to fly
-    const d = dist(frog.tongue.x, frog.tongue.y, fly.x, fly.y);
+    const d = dist(moshi.tongue.x, moshi.tongue.y, applewing.x, applewing.y);
     // Check if it's an overlap
-    const eaten = (d < frog.tongue.size/2 + fly.size/2);
+    const eaten = (d < moshi.tongue.size/2 + applewing.size/2);
     if (eaten) {
 
         // Increase score
@@ -258,9 +232,9 @@ function checkTongueFlyOverlap() {
         console.log(score);
 
         // Reset the fly
-        resetFly();
+        resetAppleFly();
         // Bring back the tongue
-        frog.tongue.state = "inbound";
+        moshi.tongue.state = "inbound";
     }
 }
 
@@ -268,8 +242,8 @@ function checkTongueFlyOverlap() {
  * Launch the tongue on click (if it's not launched yet)
  */
 function mousePressed() {
-    if (frog.tongue.state === "idle") {
-        frog.tongue.state = "outbound";
+    if (moshi.tongue.state === "idle") {
+        moshi.tongue.state = "outbound";
     }
 }
 
