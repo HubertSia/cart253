b40ba6583f36c4f2bb7c613e2732ba5f908af1ad
HubertSia <hubemail2021@gmail.com>
2024-10-24 11:23:24 -0700

    Realized the planning won't be evaluated, starting with the project

--- topics/assignments/mod-jam/js/script.js
+++ topics/assignments/mod-jam/js/script.js
@@ -2,30 +2,53 @@
  * Moshi vs Browser
  * Pippin Barr
  * 
- * A game of catching flies with your frog-tongue
- * 
- * Instructions:
- * - Move the frog with your mouse
- * - Click to launch the tongue
- * - Catch flies
- * 
- * Idea:
- * Add a score
- * 
- * 
- * Plan:
- * Every time you catch a fly a number goes by 1 ( It's a counter )
- * Display score (top right corner)
- * (Other ideas: Frog loses after three flies escape)
- * 
- * Pseudocode:
- * score = 0
- * 
- * if (the frog catches a fly)
- *     score = score + 1
- * 
- * drawScore()
- *   display the score in the tio right corner
+ * # Planning
+
+## Starting point
+
+The initial idea:
+
+> Moshi (bootleg Yoshi) eating apple-wings
+> Not letting Browser (bootleg Bowser) grapping 10 apple-wings
+
+## Experience design
+
+The experience:
+
+> The user plays as Moshi the friendly dinosaur. Their goal is to consume 10 apple-wings that will move on screen
+> by shooting the tongue with a voice mic. They have to avoid 10 misses or else Browser will win the game
+
+## Breaking it down
+
+Basic things to do:
+
+- Draw the Moshi (Bascically draw the head of bootleg Yoshi)
+- Draw Moshi's tongue
+- Move the Moshi with the mouse movement (X axis)
+- Move the apple-wings, it will spawn on both edges of the canvas (moving in a random Y Axis, and flying on a linear direction)
+- Figure out if the tongue hits the fly?
+- Adding a score points of 10
+
+Questions:
+
+- What does the Moshi look like?
+  - He is composes of a lot of circles and oval ellipse
+- How does the user control the Moshi?
+  - User controls the dinosaur with the mouse position, just to the left and right
+  - User launches the tongue with a mic sounds. They need to shout to the mic in order to launch the TONGUE
+- How does the apple-wings move?
+  - The apple-wings starts on the left at a random y position, and moves to the right in a line
+  - It will also starts on the right at a random y position, and moves to the left in a line
+- What does the tongue look like?
+  - A red line coming out from Moshi
+- What happens if the user doesn't consume the apple-wings?
+  - If the apple-wings from the left/right goes off the right/left side, it just resets to a new random y on the left/right.
+  - If the players contiues to miss 10 times. It will be a game over, showing Browser with a mencacing look
+- What does it all look like on the screen? Layout?
+  - At the start, they will be a menu screen, giving the intro information on how it works
+  - In the game, Moshi at the bottom, fly moving across, tongue shooting out of frog
+  - Moshi must consume as much apple-wings as possible before Browser reaches to 10
+  - If the players miss the apple-wings 10 times. It will be consider a game over
  * 
  * 
  * Made with p5
@@ -86,6 +109,8 @@ function draw() {
         title();
     }else if( state === "game" ){
         game();
+    }else if (state === "game-over"){
+        
     }
 }
 
