737ee90eb5358b3ae97a5f587a71106f0a27e609
HubertSia <hubemail2021@gmail.com>
2024-10-30 18:41:41 -0400

    flies-flies-flies module completed

--- topics/flies-flies/js/script.js
+++ topics/flies-flies/js/script.js
@@ -34,6 +34,13 @@ let flies = [
     }
 ];
 
+
+
+let array = [1, 2, 3, 4, 5];
+// Let's remove one element starting at index 2
+array.splice(2, 1); 
+// Now the array is [1, 2, 4, 5]
+
 /**
  * Create the canvas
  */
@@ -64,6 +71,7 @@ function draw() {
     for (let fly of flies) {
         moveFly(fly);
         drawFly(fly);
+        createFly(fly);
     }
 }
 
@@ -84,4 +92,43 @@ function drawFly(fly){
     fill(0);
     ellipse(fly.x, fly.y, fly.size);
     pop();
+}
+
+
+/**
+ * Creates a fly (data) with random numbers
+ */
+function createFly() {
+    // Generate the random fly
+    let fly = {
+        x: random(0, width),
+        y: random(0, height),
+        size: random(10, 20),
+        buzziness: random(2, 8)
+    };
+    // Return the random fly
+    return fly;
+}
+
+
+function keyPressed() {
+    let fly = createFly();
+    flies.push(fly);
+}
+
+function mousePressed() {
+    // Note we have to check *every* fly in the array to see if it was clicked
+    for (let fly of flies) {
+        // Get the distance between the click and the fly
+        let d = dist(mouseX, mouseY, fly.x, fly.y);
+        // Check if the click is inside the fly
+        if (d < fly.size/2) {
+            // If so, get the *index* of this fly in the array
+            let index = flies.indexOf(fly);
+            // And now use splice to *remove* the fly at that index
+            // Remember we provide the index and the *number of elements*
+            // to remove. In this case just the one.
+            flies.splice(index, 1);
+        }
+    }
 }
