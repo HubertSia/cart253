9d033ea5f8d149fcf7763807a0e14778089954e0
HubertSia <hubemail2021@gmail.com>
2024-10-30 16:58:13 -0400

    emptied the test template

--- topics/test-template/js/script.js
+++ topics/test-template/js/script.js
@@ -8,22 +8,4 @@
 
 "use strict";
 
-let mic;
 
-function setup() {
-  createCanvas(400, 400);
-
-  //https://p5js.org/examples/sound-mic-input.html
-  // Create an Audio input
-  mic = new p5.AudioIn();
-
-  // start the Audio Input
-  mic.start();
-}
-
-function draw() {
-  let volume = mic.getLevel()*1000;
-  background(220);
-  circle(200,200,volume)
-  console.log(volume);
-}
