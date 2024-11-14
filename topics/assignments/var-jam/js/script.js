3757d303565d44c4c29741b9a8e0b1b3ac28ef9d
HubertSia <hubemail2021@gmail.com>
2024-11-14 13:17:03 -0800

    visually seeting the keyboard piano

--- topics/assignments/var-jam/js/script.js
+++ topics/assignments/var-jam/js/script.js
@@ -35,8 +35,28 @@ function setup() {
 
 // Every frame of the program
 function draw() {
+        push();
+        rect(10, 50 , 80, 400);
+        rect(105 , 50 , 80, 400);
+        rect(200 , 50 , 80, 400);
+        rect(300 , 50 , 80, 400);
+        rect(400 , 50 , 80, 400);
+        rect(500 , 50 , 80, 400);
+        rect(600 , 50 , 80, 400);
+        pop();
 
- 
+
+
+        push();
+        fill("black");
+        rect(65 , 50 , 60, 200);
+        rect(165, 50 , 60, 200);
+        rect(360, 50 , 60, 200);
+        rect(460, 50 , 60, 200);
+        rect(560, 50 , 60, 200);
+        pop();
+
+
     
 }
 
