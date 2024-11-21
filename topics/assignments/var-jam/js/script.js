49ca610961c52561eb4e5859b2c9261b53eaa8f6
HubertSia <hubemail2021@gmail.com>
2024-11-20 19:42:53 -0500

    D

--- topics/assignments/var-jam/js/script.js
+++ topics/assignments/var-jam/js/script.js
@@ -34,14 +34,44 @@ function preload() {
  // }
 }
 
-// At the start
+/**
+ * Creates the canvas, setting up our program
+ */
 function setup() {
   createCanvas(900, 500);
   background("green");
+  
+  // Caculate the starting X to center the piano
+  let pianoWidth =  7 * whiteKeyWidth;
+  pianoX = (width - pianoWidth) / 2;
 
  }
 
 
 
+/**
+ * Updates our game states
+ */
+function draw() {
+  drawPiano();
+}
 
 
+// draw the piano
+function drawPiano() {
+
+  // Set up our loop for white keys
+  for (let i = 0; i < 7; i++) {
+    fill("white");
+    stroke("black");
+    rect(pianoX + i * whiteKeyWidth, pianoY, whiteKeyWidth, whiteKeyHeight);
+  }
+
+  //Set up our loop for black keys
+  for (let i = 0; i < 6; i++) {
+    if (i !== 2) { 
+      fill("black");
+      rect(pianoX + i * whiteKeyWidth + whiteKeyWidth * 0.72, pianoY, blackKeyWidth, blackKeyHeight);
+    }
+   }
+}
