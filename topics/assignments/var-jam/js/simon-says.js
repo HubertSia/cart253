3bc7b7af1d3a30194c5e3013820843cd10361783
HubertSia <hubemail2021@gmail.com>
2024-11-27 18:11:19 -0500

    change name of the file. Starting Simon Says var

---
+++ topics/assignments/var-jam/js/simon-says.js
@@ -0,0 +1,80 @@
+/**
+ * The piano
+ * Hubert Sia
+ */
+
+"use strict";
+
+// Array to store sound files
+let pianoNotes = [];
+
+// Key mappings to notes
+let keyMap = {
+  a: 0, // C
+  s: 1, // D
+  d: 2, // E
+  f: 3, // F
+  g: 4, // G
+  h: 5, // A
+  j: 6, // B
+};
+
+// Piano dimensions
+const pianoKeys = {
+  position: {
+    x: undefined,
+    y: 50,
+  },
+  white: {
+    w: 90,
+    h: 400,
+  },
+};
+
+// Variable to track the active key
+let activeKey = null;
+
+
+let state = 'piano';// State are title, game, game over
+
+
+
+function preload() {
+  for (let i = 0; i < 7; i++) {
+    pianoNotes[i] = loadSound(`assets/sounds/white-keys/white-keynote${i}.wav`);
+  }
+}
+
+function setup() {
+  createCanvas(900, 500);
+  background("blue");
+  pianoKeys.position.x = (width - 7 * pianoKeys.white.w) / 2;
+}
+
+function draw() {
+  drawPiano();
+}
+
+function drawPiano() {
+  for (let i = 0; i < 7; i++) {
+    fill(activeKey === i ? "red" : "white");
+    stroke("black");
+    rect(
+      pianoKeys.position.x + i * pianoKeys.white.w,
+      pianoKeys.position.y,
+      pianoKeys.white.w,
+      pianoKeys.white.h
+    );
+  }
+}
+
+function keyPressed() {
+  if (keyMap[key] !== undefined) {
+    activeKey = keyMap[key];
+    pianoNotes[activeKey].play();
+  }
+}
+
+function keyReleased() {
+  activeKey = null;
+}
